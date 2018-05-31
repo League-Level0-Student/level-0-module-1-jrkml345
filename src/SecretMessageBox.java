2259ee5455b4349b1120358bce7cca476651dee2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-31 13:25:50 -0700

    Secret Message Box,The Riddler

--- src/SecretMessageBox.java
+++ src/SecretMessageBox.java
@@ -2,6 +2,7 @@
 //    Level 0
 
 
+import javax.print.DocFlavor.STRING;
 import javax.swing.JOptionPane;
 
 /**
@@ -16,16 +17,23 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
+	public static void main(String[] args) {
+		String yoongi = "secret";
 	
 	// 1. Set a password in a String variable
-	
+	String theSecretMessage = JOptionPane.showInputDialog("what is the secret message");
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
-
+String password = JOptionPane.showInputDialog("What is the password?");
 	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
 	//    if they can guess the passcode
-
+String guest = JOptionPane.showInputDialog("Enter Password");
 	// 4. If their guess matches the password, show them the secret message
-
+if (password.equals(guest)) {
+	JOptionPane.showMessageDialog(null, theSecretMessage);
+}
 	// 5. If the password does not match, pop-up "INTRUDER!!"
-
+else {
+	JOptionPane.showMessageDialog(null,"INTRUDER!!!");
+}
+	}
 }
--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,24 +1,29 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
-
-
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+		String answer = JOptionPane.showInputDialog(
+				"You're a bus driver. 39 people got in the bus. Then 4 people got out. How old is the bus driver?");
+		// 4. If they got the answer right, pop up "correct!" and increase the score by
+		// one
+		if (answer.equals("13")) {
+			JOptionPane.showMessageDialog(null, "Yesssss");
+		}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+		else {
+			JOptionPane.showMessageDialog(null, "wrong");
+		}
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
-		
+
 	}
 }
-
