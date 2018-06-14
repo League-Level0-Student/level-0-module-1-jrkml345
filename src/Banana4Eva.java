6d0701b02afd0a0b8c0c0b71ac7db474ee8ed7c8
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-14 13:25:52 -0700

    Ive been alive, Bananas 4 eva, X geeks

---
+++ src/Banana4Eva.java
@@ -0,0 +1,10 @@
+
+public class Banana4Eva {
+public static void main(String[] args) {
+	
+	for (int i = 0; i < 1000; i++) {
+		
+		System.out.println(i + ":BANANA");
+	}
+} 
+}
--- src/XGeeks.java
+++ src/XGeeks.java
@@ -1,9 +1,6 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
-
-
 import javax.swing.JOptionPane;
 
 /*
@@ -12,14 +9,29 @@ import javax.swing.JOptionPane;
 * your program should say "June's superpower is writing recipes".
 */
 public class XGeeks {
-public static void main(String[] args) {
+	public static void main(String[] args) {
 
 		// 1. Save the superpower for each person in a variable.
-
+		String jacob = "speaks to animals";
+		String ayden = "laser eyes";
+		String tam = "power over cats";
+		String reyna = "invisibility";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
+		String name = JOptionPane.showInputDialog("Enter a name of someone at your table");
+		if (name.equals("jacob")) {
+			JOptionPane.showMessageDialog(null, jacob);
+		}
+		else if (name.equals("ayden")) {
+			JOptionPane.showMessageDialog(null, ayden);
+		}
+		else if(name.equals("tam")) {
+			JOptionPane.showMessageDialog(null, tam);
+		}
+		else if(name.equals("reyna")) {
+			JOptionPane.showMessageDialog(null, reyna);
+			
+			// 3. Show the superpower in a pop-up, depending on the name entered.
 
-		// 3. Show the superpower in a pop-up, depending on the name entered. 
-
+		}
 	}
 }
-
